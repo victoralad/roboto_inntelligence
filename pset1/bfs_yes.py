fd6980e973aa091ce7593d9be2ea4278ab8e55b4
Victor <valad@gatech.edu>
2020-08-22 19:47:57 -0400

    Do some clean up.

--- pset1/bfs_yes.py
+++ pset1/bfs_yes.py
@@ -2,38 +2,53 @@
 
 import adj_dict
 
-# Get the adjacency list from adj_dict.
-adj_dict = adj_dict.adj_dict
+def OutputResult(time, no_of_paths_popped, max_queue_size, path_cost):
+    print("Time(s): ", time)
+    print("# Paths Popped from Queue: ", no_of_paths_popped)
+    print("Max Queue Size: ", max_queue_size)
+    print("Returned path's cost: ", path_cost)
 
-# Queue to traverse the graph and append the starting point.
-queue_t = []
-queue_t.append(['WA'])
+def GetPathBFS(adj_dict, start, end):
+    # Check if the start is the goal
+    if start == end:
+        OutputResult(0, 0, 1, 0)
 
-# List to store visited nodes in the graph.
-visited_list = []
+    # Queue to traverse the graph and append the starting point.
+    queue_t = [[start]]
 
-# List to store path from origin to destination.
-path_to_goal = []
+    # List to store visited nodes in the graph.
+    visited = []
 
-# Placeholder to determine if goal has been reached.
-goal_reached = False
+    # List to store path from origin to destination.
+    path_to_goal = []
 
-# Keep searching through the graph as long as the visited list is not empty and the goal has not been reached.
-while (queue_t and goal_reached is False):
-    # print(queue_t[0], end = " ")
-    # print(adj_dict[queue_t[0]])
+    # Placeholder to determine if goal has been reached.
+    goal_reached = False
 
-    # Check if the head of the first path in the queue is the goal.
-    if queue_t[0][0] is 'GA':
-        goal_reached = True
-        break
-    # # Loop through the first node at the head of the queue.
-    # for child in adj_dict[queue_t[0]]:
-    if  queue_t[0][0] not in visited_list:
-        queue_t.append(adj_dict[queue_t[0][0]])
-        visited_list.append(queue_t[0][0])
+    # Keep searching through the graph as long as the visited list is not empty and the goal has not been reached.
+    while (queue_t and goal_reached is False):
 
-    path_to_goal.append(queue_t[0][0])
-    queue_t.pop(0)
+        # Check if the head of the first path in the queue is the goal.
+        if queue_t[0][0] == end:
+            goal_reached = True
+            break
+        # # Loop through the first node at the head of the queue.
+        # for child in adj_dict[queue_t[0]]:
+        if  queue_t[0][0] not in visited:
+            queue_t.append(adj_dict[queue_t[0][0]])
+            visited.append(queue_t[0][0])
 
-    print(queue_t)
+        path_to_goal.append(queue_t[0][0])
+        queue_t.pop(0)
+
+        # print(queue_t)
+
+if __name__ == "__main__":
+    # Get the adjacency list from adj_dict.
+    adj_dict = adj_dict.adj_dict
+
+    start = 'WA'
+    end = 'GA'
+    GetPathBFS(adj_dict, start, end)
+
+# Reference: https://pythoninwonderland.wordpress.com/2017/03/18/how-to-implement-breadth-first-search-in-python/
