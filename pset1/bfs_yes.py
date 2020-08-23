f46255712887356fcd3d7e8dc03872757f61b5d2
Victor <valad@gatech.edu>
2020-08-23 15:13:54 -0400

    Completed BFS with visited list.

--- pset1/bfs_yes.py
+++ pset1/bfs_yes.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3.6
 
+import time
 import adj_dict
 
-def OutputResult(time, no_of_paths_popped, max_queue_size, path_cost):
-    print("Time(s): ", time)
+def OutputResult(time_t, no_of_paths_popped, max_queue_size, path_cost):
+    print("Time(s): ", time_t)
     print("# Paths Popped from Queue: ", no_of_paths_popped)
     print("Max Queue Size: ", max_queue_size)
     print("Returned path's cost: ", path_cost)
 
 def GetPathBFS(adj_dict, start, end):
     # Initialize results
-    time = 0
+    time_t = 0
     no_of_paths_popped = 0
     max_queue_size = 1
     path_cost = 0
 
     # Check if the start is the goal
     if start == end:
-        OutputResult(time, no_of_paths_popped, max_queue_size, path_cost)
+        OutputResult(time_t, no_of_paths_popped, max_queue_size, path_cost)
 
     # Queue to traverse the graph and append the starting point.
     queue_t = [[start]]
@@ -25,14 +26,19 @@ def GetPathBFS(adj_dict, start, end):
     # List to store visited nodes in the graph.
     visited = []
 
+    # Get the start time
+    start_time = time.time()
+
     # Keep searching through the graph as long as the visited list is not empty and the goal has not been reached.
     while queue_t:
+        max_queue_size = max(max_queue_size, len(queue_t))
         # Remove the first path from the queue
         path = queue_t.pop(0)
+        no_of_paths_popped += 1
         # Get the last node from the path
         node = path[-1]
         if  node not in visited:
-            neighbors = adj_dict[node]
+            neighbors = sorted(adj_dict[node])
             # Loop through all the neighbors of the node.
             # Create a new path for each neighbor and add the new path to the end of the queue.
             for neighbor in neighbors:
@@ -40,10 +46,14 @@ def GetPathBFS(adj_dict, start, end):
                 new_path.append(neighbor)
                 queue_t.append(new_path)
                 if neighbor == end:
+                    path_cost = len(new_path) - 1
+                    end_time = time.time()
+                    time_t = end_time - start_time
+                    OutputResult(time_t, no_of_paths_popped, max_queue_size, path_cost)
                     return new_path
             # Mark node as visited.
             visited.append(node)
-    OutputResult('infeasible', 'infeasible', 'infeasible', 'infeasible')
+    return OutputResult('infeasible', 'infeasible', 'infeasible', 'infeasible')
     
 
 if __name__ == "__main__":
@@ -52,6 +62,7 @@ if __name__ == "__main__":
 
     start = 'WA'
     end = 'GA'
-    print(GetPathBFS(adj_dict, start, end))
+    
+    GetPathBFS(adj_dict, start, end)
 
 # Reference: https://pythoninwonderland.wordpress.com/2017/03/18/how-to-implement-breadth-first-search-in-python/
