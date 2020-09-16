663246aef821a8ef0864da7a2480ed69badfe9fd
Victor <valad@gatech.edu>
2020-09-15 20:06:42 -0400

    First attempt at the RRTs.

--- pset4/rrt.py
+++ pset4/rrt.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3.6
 
+import numpy as np
+
+class RRT:
+    def __init__(self):
+        
+        self.world = self.create_world()
+        print(np.flip(self.world, 0))
+    def create_world(self):
+
+        world = np.array([])
+
+        for i in range(10):
+            world = np.append(world, '0')
+
+        self.world = np.array([world])
+        
+        for i in range(9):
+            self.world = np.vstack([self.world, world])
+
+        # Define goal region.
+        self.world[8][8] = 'G'
+        # Define start region.
+        self.world[1][1] = 'S'
+
+        return self.world
+
 if __name__ == "__main__":
-    print("hey")
+    rrt = RRT()
+    rrt.create_world()
